1cd76627a3c0ebcf31377c3a93b1525562b2882b
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-29 18:15:45 +0530

    how to get input in java

---
+++ Taking Input/input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("enter you number: ");
+        // int num_1 =sc.nextInt();
+        // System.out.println("enter number is: " + num_1);
+        System.out.print("enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("enter name is: "+ name);
+
+    }
+}
