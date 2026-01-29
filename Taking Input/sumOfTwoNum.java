e0bab53efee207d88c4b76022a5f624c4aa1b853
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-29 18:16:05 +0530

    sum of two number fuction

---
+++ Taking Input/sumOfTwoNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sumOfTwoNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num_1: ");
+        int num_1=sc.nextInt();
+        System.out.print("enter num_2: ");
+        int num_2=sc.nextInt();
+
+        System.out.println(num_1+num_2);
+    }
+}
