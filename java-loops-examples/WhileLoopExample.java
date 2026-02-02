569e0dd70991495ffe24b1f04021881ee66ed3e7
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-02 19:49:43 +0530

    while loop in java

---
+++ java-loops-examples/WhileLoopExample.java
@@ -0,0 +1,11 @@
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        System.out.println("Printing numbers from 1 to 10:");
+        int i = 1; // Initialization
+        while (i <= 10) { // Condition
+            System.out.println(i);
+            i++; // Increment
+        }
+    }
+}
