3966f792586bfed6b306865e742c1c61339ac315
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-02 20:55:26 +0530

    Oops in java

---
+++ Oops in java/main.java
@@ -0,0 +1,12 @@
+class Student {
+    int rollNo;
+    String studentName;
+}
+
+public class main {
+    public static void main(String[] args) {
+        Student obj1=new Student();
+        System.out.println(obj1.rollNo);
+        System.out.println(obj1.studentName);
+    }
+}
