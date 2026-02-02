21250649aac0ae43dd1b66002c53bbd4de474b24
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-02 19:50:20 +0530

    for each loop in java

---
+++ java-loops-examples/ForEachLoopExample.java
@@ -0,0 +1,11 @@
+
+public class ForEachLoopExample {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Mango"};
+
+        System.out.println("Printing fruits from the array:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
