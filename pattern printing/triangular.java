0401ced520a3583027227b76dce10949785277c6
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-06 22:58:56 +0530

    pyramid

--- pattern printing/triangular.java
+++ pattern printing/triangular.java
@@ -6,6 +6,9 @@ public class triangular {
         int row =scanner.nextInt();
         for (int i = 0; i < row; i++) {
             for (int j = i; j < row; j++) {
+                System.out.print(" ");
+            }
+            for(int k =0;k<(i*2+1);k++){
                 System.out.print("*");
             }
             System.out.println();
