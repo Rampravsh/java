4820531d75ca5937acfa9e8a7ff5e1b6bfa08630
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-06 22:48:34 +0530

    reverse traingle

--- pattern printing/triangular.java
+++ pattern printing/triangular.java
@@ -5,7 +5,7 @@ public class triangular {
         Scanner scanner = new Scanner(System.in);
         int row =scanner.nextInt();
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = i; j < row; j++) {
                 System.out.print("*");
             }
             System.out.println();
