707c4ea9d157f6e37ea223a2dd1a7f522f6c550b
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-27 02:59:25 +0530

    array in java

---
+++ Arrays in java/Array1.java
@@ -0,0 +1,20 @@
+public class Array1 {
+    public static void main(String[] args) {
+        // int[] arr = new int[5]; // Declare an array of integers with size 5
+        int[] arr={1,2,3,4,5};
+
+
+        // Initialize the array with values
+        // arr[0] = 10;
+        // arr[1] = 20;
+        // arr[2] = 30;
+        // arr[3] = 40;
+        // arr[4] = 50;
+
+        // Print the elements of the array
+        System.out.println("Elements of the array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+} 
