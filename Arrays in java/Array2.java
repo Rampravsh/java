13f6d20b10fb2926254203a9010b887f1fddde25
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-27 02:59:41 +0530

    2D-array in java

---
+++ Arrays in java/Array2.java
@@ -0,0 +1,20 @@
+public class Array2 {
+    public static void main(String[] args) {
+        // int[][] matrix = new int[3][4];
+        // int x = 29;
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[i].length; j++) {
+        //         matrix[i][j] = x++;
+        //     }
+        // }
+
+        int[][] matrix={{1,2,4},{3,4,5,6}};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
