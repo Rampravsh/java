1d59ee160f2ada34717657cdf042af7f4efccb27
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-03 03:50:03 +0530

    scope of a variable

---
+++ Java Methods/method4.java
@@ -0,0 +1,26 @@
+public class method4 {
+
+    static String name ="Rampravesh";
+    public static void main(String[] args) {
+        // Scope of a variable
+
+        int x = 4; // local scope
+        int y = 5;
+
+        if (x == 4) {
+            int j = 7;
+            System.out.println(j);
+        }
+        System.out.println(x + "," + y);
+
+        System.out.println(name);
+
+        fun();
+    }
+
+    static void fun() {
+        int x = 4; // local scope
+        int y = 5;
+        System.out.println(x + "," + y);
+    }
+}
