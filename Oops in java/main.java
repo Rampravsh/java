2abd9157e62c7bf41645691d8244e81f68c7b0f0
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-04 01:42:19 +0530

    oops some basic concepts

--- Oops in java/main.java
+++ Oops in java/main.java
@@ -1,19 +1,28 @@
 class Student {
     int rollNo;
     String studentName;
+    int age;
+    String college;
+
+    void markAttendance() {
+        System.out.println("Attendance marked by " + studentName);
+    }
+
+    void print() {
+        System.out.println(studentName + "," + age + "," + college + "," + rollNo);
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Student obj1=new Student();
-        obj1.rollNo=1;
-        obj1.studentName="rampravesh";
-        System.out.println(obj1.rollNo);
-        System.out.println(obj1.studentName);
+        Student obj1 = new Student();
+        obj1.rollNo = 1;
+        obj1.studentName = "rampravesh";
+        obj1.markAttendance();
+        obj1.print();
 
-        Student obj2 =new Student();
-        obj2.rollNo=5;
-        System.out.println(obj2.rollNo);
-        
+        Student obj2 = new Student();
+        obj2.rollNo = 5;
+
     }
 }
