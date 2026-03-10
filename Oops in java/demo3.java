2f8f35789ae8a3f39b0102c68971eb15bff22968
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-10 08:03:45 +0530

    use of this keyword

--- Oops in java/demo3.java
+++ Oops in java/demo3.java
@@ -5,7 +5,11 @@ public class demo3 {
         Student s3 = new Student("shyam kumar", 19);
         Student s4 = new Student("rohit kumar", 20, 21);
         Student s5 = new Student("pankesh kumar", 22, 23, "falna college");
-
+        System.out.println(s1.name);
+        System.out.println(s2.name);
+        System.out.println(s3.name);
+        System.out.println(s4.name);
+        System.out.println(s5.name);
     }
 }
 
@@ -16,21 +20,33 @@ class Student {
     String college;
 
     Student() {
+        // this.name="unknown";
+        // this.age=0;
+        // this.rollNumber=0;
+        // this.college="unknown";
+
+        this("unknown", 0, 0, "unknown");
+
     }
 
     Student(String name) {
-        this.name = name;
+        // this.name = name;
+        this(name, 0, 0, "unknown");
     }
 
     Student(String name, int age) {
-        this.name = name;
-        this.age = age;
+        // this.name = name;
+        // this.age = age;
+
+        this(name, age, 0, "unknown");
     }
 
     Student(String name, int age, int rollNumber) {
-        this.name = name;
-        this.age = age;
-        this.rollNumber = rollNumber;
+        // this.name = name;
+        // this.age = age;
+        // this.rollNumber = rollNumber;
+
+        this(name, age, rollNumber, "unknown");
     }
 
     Student(String name, int age, int rollNumber, String college) {
