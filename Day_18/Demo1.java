7518151ffd2d8d9efdffe1a892286d8da6306da1
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-27 19:49:19 +0530

    autoboxing and unboxing

---
+++ Day_18/Demo1.java
@@ -0,0 +1,30 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        // Autoboxing
+        // int x=10;
+        // Integer y= x;
+
+        // System.out.println(x);
+        // System.out.println(y);
+
+        // Unboxing
+        // Integer a =new Integer(20);
+        // Integer a=Integer.valueOf(20);
+        // Integer a=20;
+        // int b=a;
+
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // printInteger(10);
+
+        // Integer x = null;
+        // int y = x;
+        // System.out.println(y);
+
+    }
+
+    static void printInteger(int x) {
+        System.out.println(x);
+    }
+}
