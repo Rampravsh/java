cdbf06ee0efd8c23c940de371236c9a7a18cb5c0
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-27 20:25:25 +0530

    some example of equal or not not equal Integer

---
+++ Day_18/Demo2.java
@@ -0,0 +1,12 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        // int x=10;
+        // int y=10;
+        // System.out.println(x==y);
+
+        Integer a = 10;
+        Integer b = 10;
+        System.out.println(a == b);
+
+    }
+}
